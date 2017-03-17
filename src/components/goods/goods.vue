aa8b25445e5e825478d3400cd85659dd6417413c
motif <dinghuihua@evermarker.net>
2017-03-17 19:28:39 +0800

    goods布局

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,15 +1,47 @@
 <template>
-    <div>
-       goods商品列表
+  <div class="goods">
+    <div class="menu-wrapper">
+      <div></div>
     </div>
+    <div class="foods-wrapper">
+      <ul></ul>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default{
-
+export default{
+  props: {
+    seller: {
+      type: Object
+    },
+    goods: {
+      type: Object
     }
+  },
+  created () {
+    this.$http.get('/api/goods').then(response => {
+      this.response = response.body
+      this.seller = response.seller
+      this.goods = response.goods
+    })
+  }
+}
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  .goods
+    display flex
+    position absolute
+    top 174px
+    bottom 46px
+    width 100%
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      width 80px
+      background #f3f5f7
+    .foods-wrapper
+      background #ccc
 
 </style>
