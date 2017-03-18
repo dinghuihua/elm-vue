ca9c159876c6e67876a72dbf011f9c75fb10d043
DHHding <1186991086@qq.com>
2017-03-18 18:44:19 +0800

    实现shopcart购物车组件, 购物车状态变化 依赖selectFoods数据的变化,

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <div class="tab-item">
         <a v-link="{ path:'/seller' }">商家</a></div>
     </div>
-    <router-view></router-view>
+    <router-view :seller="seller"></router-view>
   </div>
 </template>
 
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -33,11 +33,14 @@
         </li>
       </ul>
     </div>
+    <shopcart :delivery-price="seller.deliveryPrice" :min-price="seller.minPrice"></shopcart>
   </div>
 </template>
 
 <script>
   import BScroll from 'better-scroll'
+  import shopcart from 'components/shopcart/shopcart'
+
   const ERR_OK = 0
 
   export default{
@@ -110,6 +113,9 @@
         this.foodScroll.scrollToElement(el, 300)
         console.log(index)
       }
+    },
+    components: {
+      shopcart
     }
   }
 </script>
