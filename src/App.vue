b980f7cbd3cfb3413cb7cf119838bf8186c9e4e5
motif <dinghuihua@evermarker.net>
2017-03-22 11:27:22 +0800

    keep-alive  优化【route切换  组件保留  vue的生命周期不会重新开始】

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
       <div class="tab-item">
         <a v-link="{ path:'/seller' }">商家</a></div>
     </div>
-    <router-view :seller="seller"></router-view>
+    <router-view :seller="seller" keep-alive></router-view>
   </div>
 </template>
 
